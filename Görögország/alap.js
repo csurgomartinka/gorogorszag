af23f493ff8b217941bb75bd753dea7aeb84a49d
csurgomartinka <150018940+csurgomartinka@users.noreply.github.com>
2024-05-15 12:09:21 +0200

    Add files via upload

---
+++ Görögország/alap.js
@@ -0,0 +1,89 @@
+var athenjs = [
+    {
+        name: "Parthenón Pallasz Athéné temploma",
+        description: "A templom kívülről dór stílusú, legfőbb alapanyaga a sárgás színű, pentelikoni márvány. Kialakításának minden egyes részletén megfigyelhető az aprólékosság és a pontos tervezés, amelynek köszönhetően az épület még ma is megtekinthető.",
+        kep: "athenetemplom.jpg"
+    },
+    {
+        name: "Erekhtheion szentély",
+        description: "Az Akropolisz kultusztemploma volt. Rendkívül kecses, attikai ión stílus jellemzi.",
+        kep: "Erekhtheion.jpg"
+    },
+    {
+        name: "Dionüszosz színház",
+        description: "A Dionüszosz színház az egyik első színház a világon és egyben a görög dráma szülőhelye is. Egy hatalmas Dionüszosz-szobrot is állítottak az első sor elé, hogy az isten maga is láthassa az előadásokat.",
+        kep: "dionuszosz.jpg"
+    },
+    {
+        name: "Akropolisz Múzeum",
+        description: "Az Akropolisz Múzeumban az Akropolisz ásatási munkálatai során feltárt tárgyakat mutatják be. A 14.000 m2 nagyságú impozáns épületben több mint 4000 kiállítási tárgy látható.",
+        kep: "akropoliszmuzeum.jpg"
+    },
+    {
+        name: "Zeusz temploma",
+        description: "Az olümpiai Zeusz templom egykor az ókori Görögország legnagyobb szentélye volt. A görög főisten, Zeusz előtt egy aranyból és elefántcsontból készült óriási szoborral is tisztelegtek, ebből azonban sajnos semmi nem maradt, ahogy a templomból is csak ez a 15+1 oszlop, miután a 3. században, alig 100 évvel elkészülte után kifosztották, lerombolták, majd köveinek nagyrészét más építkezésekhez hordták szét.",
+        kep: "zeusz.jpg"
+    },
+    {
+        name: "Panathenaic Stadion",
+        description: "A mai stadion helyén, amely Kallimarmaro néven is ismert, már az ókorban is stadion állt. I.e. a 6. században épült abból a célból, hogy itt tarthassák meg a Pallasz Athéné istennő tiszteletére rendezett Panathénaia Játékokat, amin atlétikai számokban mérték össze tudásukat a versenyzők.",
+        kep: "panathenaic.jpg"
+    }
+];
+
+
+var szalonikijs = [
+    {
+        name: "Fehér torony",
+        description: "A vízparton uralkodó fehér torony Szaloniki egyik szimbóluma. Eredetileg az oszmánok építették, kilátást nyújt a városra és a tengerre. Jelenleg múzeumot üzemeltetnek benne.",
+        kep: "torony.jpg"
+    },
+    {
+        name: "Galerius és Rotunda íve",
+        description: "A 4. századi komplexum a római korba repít vissza minket. Az ív és a Rotunda az UNESCO Világörökség része.",
+        kep: "iv.jpg"
+    },
+    {
+        name: "Arisztotelész tér",
+        description: "A város központi tere egy nyüzsgő csomópont, amelyet kávézók, üzletek és éttermek vesznek körül.",
+        kep: "ariszter.jpg"
+    },
+    {
+        name: "Szent Demeter-bazilika",
+        description: "Az 5. században épült Szent Demeter-bazilika Szaloniki ókeresztény templomainak egyike, Szent Demeternek Szaloniki védőszentjének zarándokhelye. A templom egy öthajós emporumos és deszkatetős bazilika.",
+        kep: "szdbazilika.jpg"
+    }
+];
+
+
+function athen()
+{
+    window.location.href = "index_athen.html"
+}
+function korinthosz()
+{
+    window.location.href = "index_korinthosz.html"
+}
+function larisza()
+{
+    window.location.href = "index_larisza.html"
+}
+function patra()
+{
+    window.location.href = "index_patra.html"
+}
+function pireusz()
+{
+    window.location.href = "index_pireusz.html"
+}
+function szaloniki()
+{
+    window.location.href = "index_szaloniki.html"
+}
+function vissza()
+{
+    window.location.href = "index_varosok.html"
+}
+
+
+
